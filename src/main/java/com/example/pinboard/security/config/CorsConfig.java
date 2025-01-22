e844429510b0082652885fd634156745399b6420
Jihyeon Park <jihyeona5525@gmail.com>
2025-01-22 16:11:36 +0900

    fix: CORS cross-origins 수정3

--- src/main/java/com/example/pinboard/security/config/CorsConfig.java
+++ src/main/java/com/example/pinboard/security/config/CorsConfig.java
@@ -21,9 +21,10 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Arrays.asList(allowedOrigins.split("\\s*,\\s*")));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins.split("\\s*,\\s*")));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        config.setMaxAge(3600L);
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
